68b77db8f0373b593c02862874540cdb434f119a
Jorge Izquierdo <jorge@izqui.me>
2013-09-17 13:54:29 +0200

    imeges

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 //Port and env
-var env = process.argv[2] || 'dev';
+var env = process.argv[2] || 'prod';
 var PORT = process.env.PORT || 3000
 process.env.NODE_ENV = ((env == 'prod') ? 'production' : 'development');
-
 
 require('./server').listen(PORT, function(){
   console.log('Express server listening on port ' + PORT);
