be4873cc326d6f21b6125d8772ac5b1aec4f2bd5
Jorge Izquierdo <jorge@izqui.me>
2013-09-18 14:10:27 +0200

    test

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 var env = process.argv[2] || 'dev';
 process.env.NODE_ENV = ((env == 'prod') ? 'production' : 'development');
 
-
 var PORT = (process.env.NODE_ENV == 'production') ? 80 : 3000
 
 require('./server').listen(PORT, function(){
