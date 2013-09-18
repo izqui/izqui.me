ec9e86edd3cfe6d7acfe8dcabeae5c368a173f6c
Jorge Izquierdo <jorge@izqui.me>
2013-09-18 13:50:47 +0200

    Changed port

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //Port and env
 var env = process.argv[2] || 'dev';
-var PORT = process.env.PORT || 3000
 process.env.NODE_ENV = ((env == 'prod') ? 'production' : 'development');
+
+var PORT = (process.env.NODE_ENV == 'production') ? 80 : 3000
 
 require('./server').listen(PORT, function(){
   console.log('Express server listening on port ' + PORT+' env: '+process.env.NODE_ENV);
