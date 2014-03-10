f8754fb668a5d45421bf72bab9424eb0ea2d23d2
Jorge Izquierdo <jorge@izqui.me>
2014-03-10 19:53:10 +0100

    Add magicremote to portfolio

--- app.js
+++ app.js
@@ -14,5 +14,5 @@ mongoose.model('Message', model.message)
 //Server boot
 var PORT = (process.env.NODE_ENV == 'production') ? 8000 : 8080
 require('./server').listen(PORT, function(){
-  console.log('Express server listening on port ' + PORT+' env: '+process.env.NODE_ENV);
+  console.log('Express server listening on port ' + PORT +' env: '+process.env.NODE_ENV);
 });
